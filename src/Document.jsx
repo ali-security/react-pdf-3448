100a4f4b2d4e4e1ebb7b096ce55ea341274f6440
Seal Security <devops-admin@sealsecurity.io>
2025-10-15 09:16:41 +0000

    patched v6.2.2

--- src/Document.jsx
+++ src/Document.jsx
@@ -115,7 +115,9 @@ export default class Document extends PureComponent {
 
         const { options, onLoadProgress, onPassword } = this.props;
 
-        const destroyable = pdfjs.getDocument({ ...source, ...options });
+        const optionsWithIsEvalSupportedDisabled = { ...options, isEvalSupported: false };
+
+        const destroyable = pdfjs.getDocument({ ...source, ...optionsWithIsEvalSupportedDisabled });
         destroyable.onPassword = onPassword;
         if (onLoadProgress) {
           destroyable.onProgress = onLoadProgress;
